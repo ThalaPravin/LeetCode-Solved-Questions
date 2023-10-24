5e125a1d729f598baad59da9785dd2ee8b6cdd27
ThalaPravin <122007931+ThalaPravin@users.noreply.github.com>
2023-10-24 10:56:06 -0700

    Time: 16 ms (37.93%), Space: 20.2 MB (6.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,10 +4,10 @@ public:
         
         map<int,int>m;
         
-        for(int i=0; i<nums.size(); i++) 
+        for(auto i: nums) 
         {
-            m[nums[i]]++;
-            if(m[nums[i]]>nums.size()/2) return nums[i];
+            m[i]++;
+            if(m[i]>nums.size()/2) return i;
         }
         
         return 0;
