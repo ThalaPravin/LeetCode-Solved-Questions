3bde83497fe191a6a85898edad1d57cbcee99706
Pravin Avhad <122007931+ThalaPravin@users.noreply.github.com>
2024-06-25 16:10:14 +0530

    Time: 3 ms (22.93%), Space: 7.1 MB (81.63%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        if(n<1) return 0;
-        if(n<3) return 1;
-        int dp[n+1];
+        
+        int dp[n+3];
         dp[0]=0;
         dp[1]=dp[2]=1;
 
