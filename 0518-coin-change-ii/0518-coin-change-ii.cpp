4a7cf4c01c6da317a602c6e7b8a90791d6678b7b
Pravin Avhad <122007931+ThalaPravin@users.noreply.github.com>
2024-06-26 18:11:22 +0530

    Time: 7 ms (71.53%), Space: 13.1 MB (79.98%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+         int n=coins.size();
+
+        int dp[n+1][amount+1];
+        memset(dp, -1, sizeof(dp));
+
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<amount+1; j++){
+                if(i==0 ) dp[i][j]=0;
+               
+            }
+        }
+
+         for(int i=0; i<n+1; i++){
+            for(int j=0; j<amount+1; j++){
+                if(j==0 ) dp[i][j]=1;
+               
+            }
+        }
+
+        for(int i=1; i<n+1; i++){
+            for(int j=0; j<amount+1; j++){
+                if(coins[i-1]<=j){
+                    dp[i][j]= dp[i][j-coins[i-1]] + dp[i-1][j]  ;
+                }
+                else{
+                    dp[i][j]= dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
+};
