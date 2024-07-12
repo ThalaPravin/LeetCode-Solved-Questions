ff6cdc0c7c5dff3426c679500bd5361c03079c30
Pravin Avhad <122007931+ThalaPravin@users.noreply.github.com>
2024-07-12 10:04:29 +0530

    Time: 9 ms (48.02%), Space: 27.4 MB (8.25%) - LeetHub

--- 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.cpp
+++ 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.cpp
@@ -1,15 +1,43 @@
 class Solution {
 public:
     int maximumStrongPairXor(vector<int>& nums) {
-        int mx=INT_MIN;
-        int n=nums.size();
-        for(int i=0;i<n; i++){
-            for(int j=i; j<n; j++){
-                if(abs(nums[i]-nums[j])<= min(nums[i], nums[j])){
-                    mx=max(mx, nums[i] ^ nums[j]);
-                }
+        // Brute Force
+        // int mx=INT_MIN;
+        // int n=nums.size();
+        // for(int i=0;i<n; i++){
+        //     for(int j=i; j<n; j++){
+        //         if(abs(nums[i]-nums[j])<= min(nums[i], nums[j])){
+        //             mx=max(mx, nums[i] ^ nums[j]);
+        //         }
+        //     }
+        // }
+        // return mx;
+
+
+        // Sliding Window O(n *logn)
+
+        int l = 0 , r = 0 , total , maxTotal = 0;
+        if (nums.size() < 2)
+        {
+            return 0;
+        }
+        for(; l < nums.size() - 1 ; l++)
+        {
+            if((abs(nums[l] - nums[r])) <= min(nums[l] , nums[r]))
+            {
+                total = nums[l] ^ nums[r];
+                maxTotal = max(total, maxTotal);
+            }
+            if(r == nums.size()-1)
+            {
+                r = l + 1;
+            }
+            else
+            {
+                l--;
+                r++;
             }
         }
-        return mx;
+        return maxTotal;
     }
 };
