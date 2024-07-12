6f7043776c5b4f533fe329b1fefc5e05105d83f5
Pravin Avhad <122007931+ThalaPravin@users.noreply.github.com>
2024-07-12 23:54:01 +0530

    Time: 4 ms (55.28%), Space: 14.8 MB (16.12%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    template<class BidirIt>
-bool next_permutation(BidirIt first, BidirIt last)
-{
-    auto r_first = std::make_reverse_iterator(last);
-    auto r_last = std::make_reverse_iterator(first);
-    auto left = std::is_sorted_until(r_first, r_last);
- 
-    if (left != r_last)
-    {
-        auto right = std::upper_bound(r_first, left, *left);
-        std::iter_swap(left, right);
-    }
- 
-    std::reverse(left.base(), last);
-    return left != r_last;
-}
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+      
+        int idx = -1;
+        for(int i = n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                idx = i;
+                break;
+            }
+        }
+
+        if(idx==-1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        else{
+            reverse(nums.begin()+idx+1, nums.end());
+
+            int j = -1;
+        for(int i=idx+1; i<n; i++){
+            if(nums[idx]<nums[i]){
+                j = i;
+                break;
+            }
+        }
+        
+        int temp = nums[idx];
+        nums[idx]= nums[j];
+        nums[j]=temp;
+        return;
+        }
+        
     }
 };
