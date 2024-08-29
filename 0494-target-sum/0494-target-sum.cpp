19061d97c87282a7abfe50cc176d750b0b58f74b
Pravin Avhad <122007931+ThalaPravin@users.noreply.github.com>
2024-08-29 22:38:56 +0530

    Time: 7 ms (71.67%), Space: 14 MB (53.56%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-     int subsetsumDiff(vector <int> arr, int n , int sum){
+    int findTargetSumWays(vector<int>& arr, int d) {
+         int tsum=0, n=arr.size();
+        for(int i: arr) tsum+=i;
+        
+        if((tsum-d)%2!=0 || tsum<d ) return 0;
+        
+        return perfectSum(arr, n, (tsum-d)/2);
+    }
   
-	    int dp[n+1] [sum+1];
-	    
-	    memset(dp, 0, sizeof(dp));
-	    
-	    for(int i=0; i<n+1; i++){
-	        dp[i][0]=1;
-	    }
-        // if (arr[0]==0) dp[0][0] = 2;
-	    
-	    for(int i=1; i<n+1; i++){
-	        for(int j=0; j<sum+1; j++){
-	            
-	            if(arr[i-1]<=j){
-	                dp[i][j]= dp[i-1][j-arr[i-1]]  + dp[i-1][j];
-	            }
-	            else{
-	                dp[i][j]= dp[i-1][j];
-	            }
-	        }
-	    }
-
-        return dp[n][sum];
-     }
-
-    int findTargetSumWays(vector<int>& arr, int target) {
-        long long sum=0;
-        int n=arr.size();
-	    for(int i=0; i<n; i++){
-	        sum+=arr[i];
-	    }
-        int s=(target+sum)/2;
-
-        if(sum<abs(target)||(sum+target)%2!=0)
+    int f(vector<vector<int>>&dp,vector<int>& arr, int n, int sum,int i){
+        if(sum==0 && i==n){
+            return 1;
+        }
+        
+        if(sum<0 || i==n)
             return 0;
-
-        int count=subsetsumDiff(arr, n, abs(s));
-        return count ;
+        
+        if(dp[i][sum]!=-1)
+            return dp[i][sum];
+        
+        
+        
+        int notTake=f(dp,arr,n,sum,i+1);
+        int take=0;
+        if(arr[i]<=sum)
+            take=f(dp,arr,n,sum-arr[i],i+1);
+        
+        return dp[i][sum]=(notTake+take);
+    }
+    
+    int perfectSum(vector<int>& arr, int n, int sum)
+    {
+        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
+        return f(dp,arr,n,sum,0);
     }
 };
