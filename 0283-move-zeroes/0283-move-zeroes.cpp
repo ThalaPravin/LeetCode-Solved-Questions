6f535e4ee056d36e59694096dd471197945d7d99
Pravin Avhad <122007931+ThalaPravin@users.noreply.github.com>
2025-01-15 12:22:01 +0530

    Time: 0 ms (100%), Space: 23.7 MB (67.12%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,18 +3,22 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
         vector<int>temp;
-        int cnt=0;
-        for(int i:nums){
-            if(i!= 0) temp.push_back(i);
-            else cnt++;
+        int j=-1;
+
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
         }
 
-        for(int i=0; i<n-cnt; i++){
-            nums[i]=temp[i];
-        }
+        if (j==-1) return ;
 
-        for(int i=0; i<cnt; i++){
-            nums[n-cnt+i]=0;
+        for(int i=j+1; i<n; i++){
+            if(nums[i]!=0){
+                swap(nums[i], nums[j]);
+                j++;
+            }
         }
     }
 };
