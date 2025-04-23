dc9dbb0c969439003cf74efad3af45ce76ba7138
Pravin Avhad <122007931+ThalaPravin@users.noreply.github.com>
2025-04-23 12:12:00 +0530

    Time: 5 ms (8.47%), Space: 7.9 MB (74.92%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findDigitSum(int n){
+        int sum=0;
+        while(n){
+            int temp=n%10;
+            n/=10;
+            sum+=temp;
+        }
+        return sum;
+    }
+    int countLargestGroup(int n) {
+        map<int,int>mp;
+        int mx=0;
+
+        for(int i=1; i<=n; i++){
+            int temp= findDigitSum(i);
+
+            if(mp.find(temp)!=mp.end()){
+                mp[temp]++;
+            }
+            else{
+                mp[temp]=1;
+            }
+            mx= max(mx,mp[temp]);
+        }
+
+        int ans=0;
+        for(auto i:mp){
+            if(i.second == mx){
+                ans++;
+            }
+        }
+
+        return ans;
+
+
+    }
+};
