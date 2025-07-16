5d71c301c5ce00d3496a21e4beafbf83d781ee38
Pravin Avhad <122007931+ThalaPravin@users.noreply.github.com>
2025-07-16 14:19:23 +0530

    Time: 75 ms (30.92%), Space: 25.7 MB (34.44%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -3,37 +3,41 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size();
         int m=heights[0].size();
+        if (n==1 && m==1) return 0;
         vector<vector<int>>distance(n+1,vector<int>(m+1,INT_MAX));
         vector<vector<int>>vis(n+1,vector<int>(m+1,0));
-        priority_queue<
-        pair<pair<int, int>, pair<int, int>>, 
-        vector<pair<pair<int, int>, pair<int, int>>>, 
-        greater<pair<pair<int, int>, pair<int, int>>>>pq;
-        pq.push(make_pair(make_pair(0,heights[0][0]), make_pair(0,0)));
+        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
+
+        pq.push({0,{0,0}});
         vector<int>dx{0,0,1,-1};
         vector<int>dy{-1,1,0,0};
-        while(pq.size()!=0){
-            int current_cell_value=pq.top().first.second;
-            int current_mx=pq.top().first.first;
-            int row=pq.top().second.first;
-            int col=pq.top().second.second;
-            vis[row][col]=1;
+
+        int ans=INT_MAX;
+    
+        while(!pq.empty()){
+            auto it = pq.top();
+            int dis= it.first;
+            int row= it.second.first;
+            int col = it.second.second;
             pq.pop();
-            if(row==n-1 && col==m-1){
-                return current_mx;
-            }
-            for(int i=0;i<4;i++){
-                if(row+dx[i]>=0 && row+dx[i]<n && col+dy[i]>=0 && col+dy[i]<m ){
-                    int next_row=row+dx[i];
-                    int next_col=col+dy[i];
-                    int next_cell_value=heights[next_row][next_col];
-                    if(abs(current_cell_value-next_cell_value)<distance[next_row][next_col]){
-                        distance[next_row][next_col]=abs(current_cell_value-next_cell_value);
-                        pq.push(make_pair(make_pair(max(current_mx,distance[next_row][next_col]), heights[next_row][next_col]), make_pair(next_row,next_col)));
+           
+
+            for(int i=0; i<4; i++){
+                int newr= dx[i] +row;
+                int newc = dy[i]+col;
+
+                if(newr>=0 && newr<n && newc>=0 && newc<m){
+                    int neweff = max(dis, abs(heights[row][col]- heights[newr][newc]));
+
+                    if(neweff < distance [newr][newc]){
+                        distance[newr][newc]= neweff;
+                        pq.push({neweff,{newr,newc}});
                     }
                 }
             }
         }
-        return 0;
+
+        return distance[n-1] [m-1];
+      
     }
 };
